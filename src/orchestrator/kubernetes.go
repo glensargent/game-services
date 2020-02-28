645233d0e05185614017140b419ebc74307eeea4
Glen Sargent <glensarg@hotmail.co.uk>
2020-02-28 12:21:02 +0000

    create a pod

--- src/orchestrator/kubernetes.go
+++ src/orchestrator/kubernetes.go
@@ -1,26 +1,51 @@
 package main
 
 import (
-	"log"
+	"encoding/json"
+	"fmt"
 
+	apiv1 "k8s.io/api/core/v1"
+	v1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
-type k8s struct {
+type cluster struct {
+	config    *rest.Config
+	clientset *kubernetes.Clientset
 }
 
-func makeCluster() {
+func newCluster() cluster {
+	return cluster{}
+}
+
+func (c *cluster) init() {
 	// creates the in-cluster config
-	config, err := rest.InClusterConfig()
+	var err error
+	c.config, err = rest.InClusterConfig()
 	if err != nil {
-		panic(err.Error())
-	}
-	// creates the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
+		// no point exiting if it can't connect to the cluster
 		panic(err.Error())
 	}
 
-	log.Println(clientset)
+	// creates the clientset
+	c.clientset, err = kubernetes.NewForConfig(c.config)
+	if err != nil {
+		// no point exiting if it can't connect to the cluster
+		panic(err.Error())
+	}
+}
+
+func (c cluster) makePod(yaml []byte) error {
+	var p v1.Pod
+	err := json.Unmarshal([]byte(yaml), &p)
+	if err != nil {
+		return err
+	}
+	pod, poderr := c.clientset.CoreV1().Pods(apiv1.NamespaceDefault).Create(&p)
+	if poderr != nil {
+		return poderr
+	}
+	fmt.Printf("Created Pod %q.\n", pod.GetObjectMeta().GetName())
+	return nil
 }
--- src/orchestrator/main.go
+++ src/orchestrator/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
 	handler := makeRouter()
+	cluster := newCluster()
+	cluster.init()
 
-	makeCluster()
+	bytes, err := ioutil.ReadFile("pod.json")
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		log.Println("attempting to make a pod")
+		cluster.makePod(bytes)
+	}()
 
 	log.Println("listening to port 8080")
 	http.ListenAndServe(":8080", handler)
